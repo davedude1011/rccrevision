601b61979c704b04a7005440497be97d269c9011
davedude1011 <davedude.productions.inc@gmail.com>
2024-08-21 13:31:16 +0100

    db keys

---
+++ src/server/serverFile.ts
@@ -0,0 +1,12 @@
+import { drizzle } from 'drizzle-orm/vercel-postgres';
+import { sql } from "@vercel/postgres";
+import {
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  uniqueIndex,
+} from 'drizzle-orm/pg-core';
+
+// Use this object to send drizzle queries to your DB
+export const db = drizzle(sql);
